6a2f602b36cc6dfbe9bcc7004fe9ce8678311f77
panzi24 <simonaco777@gmail.com>
2026-09-18 18:41:53 +0200

    Actualizacion del scnner

--- src/scanner/Main.java
+++ src/scanner/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		String linea = "-".repeat(40);
 		
 		
 		System.out.println("¿Como te llamas?: ");
@@ -14,10 +15,22 @@ public class Main {
 		
 		System.out.println("¿Y tu edad?: ");
 		int edad = sc.nextInt();
+		sc.nextLine();
 		
 		
 		
-		System.out.printf("Te llamas %s y tu edad es %d", nombre ,edad);
+		
+		
+		System.out.println(linea);
+		
+		int numeroBiblio = 0;
+		
+		System.out.println("¿Cuantos libros hay en tu biblioteca?: ");
+		
+		numeroBiblio = Integer.parseInt(sc.nextLine());
+		System.out.printf("Te llamas %s y tu edad es %d%n", nombre ,edad);
+		System.out.println(linea);
+		System.out.printf("En tu biblio hay %d libros", numeroBiblio);
 		
 		
 	}
