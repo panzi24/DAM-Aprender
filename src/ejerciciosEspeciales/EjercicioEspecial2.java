fa601140cf71b5f113f299831508e63e21aa843c
panzi24 <simonaco777@gmail.com>
2026-09-22 01:46:52 +0200

    Pendiente el ejercicio especial II: Calculamos distancia = tiempo * velocidad, sacar la hora estimada la parcial, printf

---
+++ src/ejerciciosEspeciales/EjercicioEspecial2.java
@@ -0,0 +1,29 @@
+package ejerciciosEspeciales;
+
+public class EjercicioEspecial2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Troposfera
+		int distanciaTroposfera = 15 - 0;
+		int velocidadTroposfera = 1;
+		int tiempoTroposfera = distanciaTroposfera / velocidadTroposfera;
+		
+		//Estratosfera
+		int distanciaEstratosfera = 50 - 16;
+		int velocidadEstratosfera = 2;
+		int tiempoEstratosfera = distanciaEstratosfera / velocidadEstratosfera;
+		
+		//Mesosfera
+		int distanciaMesosfera = 80 - 51;
+		int velocidadMesosfera = 4;
+		int tiempoMesosfera = distanciaMesosfera / velocidadMesosfera;
+		
+		
+		
+		
+		
+	}
+
+}
